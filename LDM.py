bce806ea007a61a300080c668f20c77e24e9bc31
div-thombare <164990221+div-thombare@users.noreply.github.com>
2024-10-01 03:37:55 -0700

    Create LDM.py

---
+++ LDM.py
@@ -0,0 +1,17 @@
+# importing required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+ 
+# reading csv file and extracting class column to y.
+x = pd.read_csv("C:\...\cancer.csv")
+a = np.array(x)
+y  = a[:,30] # classes having 0 and 1
+ 
+# extracting two features
+x = np.column_stack((x.malignant,x.benign))
+ 
+# 569 samples and 2 features
+x.shape 
+ 
+print (x),(y)
